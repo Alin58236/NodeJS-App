75c89ff9ab545bd5acd88528e377f630fd75e08e
Stoia <alin58236@gmail.com>
2022-09-27 22:25:42 +0300

    Lesson 7 - Adds views, partials and dynamic data

--- Lesson 5/app.js
+++ Lesson 5/app.js
@@ -1,3 +1,5 @@
+//----------------------------INITIALIZATION-------------------------------------//
+
 const express = require('express');
 
 //express app
@@ -8,32 +10,59 @@ app.set('view engine', 'ejs');
 
         //app.set('views', 'myviews') - to set a custom views folder ( the standard folder is 'views', which we already made)
 
-
 //listen for reqests
 app.listen(3000);
 
+
+//----------------------------------PAGES----------------------------------------//
+
+
 app.get('/', (req,res) =>{
+    
     //res.send('<p>Home Page!!!!</p>');
     //res.sendFile('./views/index.html', { root:__dirname });
 
-    res.render('index'); //to render a view (we are not sending html anymore, instead we render views)
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet:'The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog.'},
+        {title: 'Mario finds stars', snippet:'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.'},
+        {title: 'How to defeat browser', snippet:'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born.'},
+    ]
+
+
+    res.render('index', {title: 'Homepage', blogs}); //to render a view (we are not sending html anymore, instead we render views)
 
 });
+
+
+
 
 app.get('/about', (req,res) =>{
     //res.send('<p>About Page</p>');
     //res.sendFile('./views/about.html', { root:__dirname });
-    res.render('about');
+    res.render('about', {title: 'About'});
 });
 
-//redirects
-
+//redirect
 app.get('/about-us',(req,res) => {
-    res.render('/about');
+    res.render('about', {title: 'AbOuT'});
 });
 
-//404 page
+
+
+
+
+
+app.get('/blogs/create', (req,res) =>{
+    //res.send('<p>About Page</p>');
+    //res.sendFile('./views/about.html', { root:__dirname });
+    res.render('create', {title: 'Create a post'});
+});app
+
+
+
+
+//--------------------------------404-page----------------------------------//
 
 app.use((req,res) => {
-    res.status(404).render('404');
+    res.status(404).render('404',{title: 'Error Page'});
 });
