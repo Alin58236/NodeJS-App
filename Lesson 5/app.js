908a3b24fef1c1f4372a5210d6e134ac435b40aa
Stoia <alin58236@gmail.com>
2022-09-27 12:42:03 +0300

    Created view engines for dynamic data and CSS

--- Lesson 5/app.js
+++ Lesson 5/app.js
@@ -3,27 +3,37 @@ const express = require('express');
 //express app
 const app = express();
 
+//register view engine
+app.set('view engine', 'ejs');
+
+        //app.set('views', 'myviews') - to set a custom views folder ( the standard folder is 'views', which we already made)
+
+
 //listen for reqests
 app.listen(3000);
 
 app.get('/', (req,res) =>{
     //res.send('<p>Home Page!!!!</p>');
-    res.sendFile('./views/index.html', { root:__dirname });
+    //res.sendFile('./views/index.html', { root:__dirname });
+
+    res.render('index'); //to render a view (we are not sending html anymore, instead we render views)
+
 });
 
 app.get('/about', (req,res) =>{
     //res.send('<p>About Page</p>');
-    res.sendFile('./views/about.html', { root:__dirname });
+    //res.sendFile('./views/about.html', { root:__dirname });
+    res.render('about');
 });
 
 //redirects
 
 app.get('/about-us',(req,res) => {
-    res.redirect('/about');
+    res.render('/about');
 });
 
 //404 page
 
 app.use((req,res) => {
-    res.sendFile('./views/404.html', {root: __dirname});
+    res.status(404).render('404');
 });
