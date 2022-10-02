e3a2298f05f0b32dedf7f41d34bdeb9090818cd9
Stoia <alin58236@gmail.com>
2022-10-03 01:56:07 +0300

    Inserts the Blogs type documents into the HTML document

--- Lesson 5/app.js
+++ Lesson 5/app.js
@@ -3,15 +3,16 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose= require('mongoose');
-
+const Blog = require('./models/blog');
+const { result } = require('lodash');
 
 //express app
 const app = express();
 
 //connect to MongoDB
 
-const dbURI = 'mongodb+srv://user:test1234@nodetuts.fwm8l8c.mongodb.net/?retryWrites=true&w=majority';
-mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+const dbURI = 'mongodb+srv://user:test1234@nodetuts.fwm8l8c.mongodb.net/node-tuts?retryWrites=true&w=majority';
+mongoose.connect(dbURI)
     .then((result) => {
         //listen for reqests only after the connection to the database is established
         app.listen(3000);
@@ -20,6 +21,9 @@ mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
     .catch((err) => {
         console.log(err);
     })
+
+    
+
 
 
 //register view engine
@@ -43,7 +47,7 @@ app.use((req,res,next) => {
 });
 
 app.use((req,res,next) => {
-    console.log('In the next middleware');
+    console.log('Middleware done');
     next();
 });
 
@@ -56,25 +60,8 @@ app.get('/', (req,res) =>{
     //res.send('<p>Home Page!!!!</p>');
     //res.sendFile('./views/index.html', { root:__dirname });
 
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet:'The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog.'},
-        {title: 'Mario finds stars', snippet:'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.'},
-        {title: 'How to defeat browser', snippet:'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born.'},
-    ]
-
-
-    res.render('index', {title: 'All Blogs', blogs}); //to render a view (we are not sending html anymore, instead we render views)
-
+    res.redirect('blogs');
 });
-
-app.get('/blogs', (req,res) =>{
-    //res.send('<p>About Page</p>');
-    //res.sendFile('./views/about.html', { root:__dirname });
-    res.render('index', {title: 'All Blogs'});
-});
-
-
-
 app.get('/about', (req,res) =>{
     //res.send('<p>About Page</p>');
     //res.sendFile('./views/about.html', { root:__dirname });
@@ -83,12 +70,27 @@ app.get('/about', (req,res) =>{
 
 //redirect
 app.get('/about-us',(req,res) => {
-    res.render('about', {title: 'AbOuT'});
+    res.render('about', {title: 'About-Us'});
 });
 
 
+    //blog routes
 
+app.get('/blogs', (req,res) =>{
+    
+    //res.send('<p>About Page</p>');
+    //res.sendFile('./views/about.html', { root:__dirname });
 
+    Blog.find().sort({createdAt: -1})
+    .then((result) =>{
+        res.render('index', {title: 'All Blogs', blogs: result} );
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+    
+});
 
 
 app.get('/blogs/create', (req,res) =>{
@@ -97,11 +99,7 @@ app.get('/blogs/create', (req,res) =>{
     res.render('create', {title: 'Create a post'});
 });
 
-app.get('/create', (req,res) =>{
-    //res.send('<p>About Page</p>');
-    //res.sendFile('./views/about.html', { root:__dirname });
-    res.render('create', {title: 'Create a post'});
-});
+
 
 //--------------------------------404-page----------------------------------//
 
