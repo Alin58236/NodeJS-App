9673aac48d3c107493a7f15823f65c060e8b27bc
Stoia <alin58236@gmail.com>
2022-12-12 22:25:12 +0200

    Fix CSS errors and routing errors

--- Lesson 5/app.js
+++ Lesson 5/app.js
@@ -9,17 +9,23 @@ const { render } = require('ejs');
 const path = require('path');
 
 
+const User = require('./models/User')
+
+
 //express app
 const app = express();
 
 //connect to MongoDB
 
 const dbURI = 'mongodb+srv://user:test1234@nodetuts.fwm8l8c.mongodb.net/node-tuts?retryWrites=true&w=majority';
+
+const PORT=2827
+
 mongoose.connect(dbURI)
     .then((result) => {
         //listen for reqests only after the connection to the database is established
-        app.listen(3000);
-        console.log("Connected to MongoDB on port 3000");
+        app.listen(PORT);
+        console.log("Connected to MongoDB on port "+PORT);
     })
     .catch((err) => {
         console.log(err);
@@ -38,23 +44,24 @@ app.set('view engine', 'ejs');
 
 //middleware and static files
 
+
+                
+
 //app.use(express.static('public'));
 
 app.use(express.static(path.join(__dirname,'public')))
 
 app.use(express.urlencoded({extended: true})); // for posting form data
+
 app.use(morgan('dev'));
 
 app.use((req,res,next) => {
+    console.log()
     console.log('<-------------------------------------------> New request made:');
     console.log('host: ', req.hostname);
     console.log('path: ', req.path);
     console.log('method: ', req.method);
-    next();
-});
-
-app.use((req,res,next) => {
-    console.log('===Middleware done===');
+    console.log()
     next();
 });
 
@@ -62,13 +69,14 @@ app.use((req,res,next) => {
 //----------------------------------PAGES----------------------------------------//
 
 
-app.get('/', (req,res) =>{
-    
-    //res.send('<p>Home Page!!!!</p>');
-    //res.sendFile('./views/index.html', { root:__dirname });
+ app.get('/', (req,res) =>{
 
-    res.redirect('blogs');
-});
+      res.redirect('blogs');
+
+ });
+
+
+
 app.get('/about', (req,res) =>{
     //res.send('<p>About Page</p>');
     //res.sendFile('./views/about.html', { root:__dirname });
@@ -81,7 +89,8 @@ app.get('/about-us',(req,res) => {
 });
 
 
-    //blog routes
+
+//blog routes
 
 app.get('/blogs', (req,res) =>{
     
@@ -141,7 +150,6 @@ app.post('/blogs', (req,res) => {
 })
 
 
-
 //blog delete request
 app.delete('/blogs/:id' , (req,res) => {
     
@@ -156,11 +164,8 @@ app.delete('/blogs/:id' , (req,res) => {
     })
 })
 
-
-
-
 //--------------------------------404-page----------------------------------//
 
-app.use((req,res) => {
-    res.status(404).render('404',{title: 'Error Page'});
-});
+ app.use((req,res) => {
+     res.status(404).render('404',{title: 'Error Page'});
+ });
